18ed99092ef1ea9e86c3870cd07bfd0035ec1071
v2s-Rakesh <rakesh.h@v2solutions.com>
2015-08-14 14:49:41 +0530

    changed code( program takes cmd line args) and added testing code

--- triangle_1.rb
+++ triangle_1.rb
@@ -1,26 +1,36 @@
+
+require "test/unit"
+
 class Triangletype1
-	def triangle_1(n)
-	    num=line=1
-	    p=[1]
-	    pt=Array.new(n*2-line+1," ")
-	    pt.each{ |x| print x }
-	    puts p.join(" ")
-	    while(p.length<n*2)
-	        pt=Array.new(n*2-line," ")
-	        c= pt+p
-
-	        p=Array.new(p.length+2) {|i| 
-	          a=i<=line ? num+=1 : num-=1
-	          a}
-			
-			line+=1
-
-	        pt.each{ |x| print x }
-	        puts p.join(" ")
-	        puts
-	    end
-	end
+    def triangle_1(i,n)
+        num=i-1
+        line=[" "]
+            line.push(" "*(n*2-i*2))
+            (i*2-1).times do |i| 
+                if num==9
+                    line.push(num=0)
+                else
+                    line.push(num+=1)
+                end
+            end
+        
+        return line
+    end
 end
 
-triangle=Triangletype1.new
-triangle.triangle_1(10)
+class TestNumbers < Test::Unit::TestCase
+    def test_odds
+        assert_equal([" ", "", 5, 6, 7, 8, 9, 0, 1, 2, 3], Triangletype1.new.triangle_1(5,5))
+    end
+
+end
+
+n=ARGV[0].to_i
+
+tri=Triangletype1.new
+n.times do |i|
+    
+    line=tri.triangle_1(i,n)
+    puts line.join(" ")
+end
+
--- triangle_2.rb
+++ triangle_2.rb
@@ -1,26 +1,49 @@
+
+require "test/unit"
+
 class Triangletype2
-    def triangle_2(n)
-        num=line=1
-        p=[1]
-        pt=Array.new(n*2-line+1," ")
-        pt.each{ |x| print x }
-        puts p.join(" ")
-        while(p.length<n*2)
-            pt=Array.new(n*2-line," ")
-            c= pt+p
+    def triangle_1(i,n)
+        num=i-1
+        line=[" "]
+            line.push(" "*(n*2-i*2))
+            i.times do |i| 
+                if num==9
+                    line.push(num=0)
+                else
+                    line.push(num+=1)
+                end
+            end
+        if i-1>0
+            (i-1).times do |i|  
+                if num==9
+                    line.push(num=8)
+                else
+                    if num==0
+                        line.push(num=9)
 
-            p=Array.new(p.length+2) {|i| 
-              a=i<=line ? num+=1 : num+=1
-              a}
-    		
-    		line+=1
-
-            pt.each{ |x| print x }
-            puts p.join(" ")
-            puts
+                    end
+                    
+                line.push(num-=1)
+                end
+            end
         end
+        return line
     end
 end
 
-triangle=Triangletype2.new
-triangle.triangle_2(10)
+class TestNumbers < Test::Unit::TestCase
+    def test_line
+        assert_equal([" ", "", 5, 6, 7, 8, 9, 8, 7, 6, 5], Triangletype2.new.triangle_1(5,5))
+    end
+
+end
+
+n=ARGV[0].to_i
+
+tri=Triangletype2.new
+n.times do |i|
+    
+    line=tri.triangle_1(i,n)
+    puts line.join(" ")
+end
+
