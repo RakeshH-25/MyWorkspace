9b2552a1343b441fec71c0ca62077f4443a48a3e
v2s-Rakesh <rakesh.h@v2solutions.com>
2015-08-17 18:44:43 +0530

    for MxT-MnT

--- FileExample.rb
+++ FileExample.rb
@@ -3,25 +3,23 @@ require "test/unit"
 class Whether
 	
 	def get_day(path)
-		col3 = []
+		temp_diff = []
 		col1 = []
 		File.open(path, "r").each_line do |line|
 		  	#puts line
 		  	data = line.split(/\s+/)
-		  	 col3 << data[3] if ( data[3].to_i > 0)
-		  	 col1 << data[1] if ( data[3].to_i > 0)
+		  	 temp_diff << data[2].to_i-data[3].to_i if (data[1].to_i > 0 && data[3].to_i > 0)
+		  	 #col1 << data[1] if ( data[3].to_i > 0)
 		end
 
-		day_index = col3.index(col3.min)
-		
-		col1[day_index.to_i]
+		temp_diff[temp_diff.min]
 	end
 end
 
 class TestNumbers < Test::Unit::TestCase
     def test_line
     	file="/home/rakesh/Downloads/weather.txt"
-        assert_equal("9", Whether.new.get_day(file))
+        assert_equal(22, Whether.new.get_day(file))
     end
 
 end
