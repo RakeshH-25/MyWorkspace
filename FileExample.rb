3b534bff990a31752b38ae845763f90b3b426f3e
v2s-Rakesh <rakesh.h@v2solutions.com>
2015-08-17 18:27:02 +0530

    Modified as per comments

--- FileExample.rb
+++ FileExample.rb
@@ -3,18 +3,18 @@ require "test/unit"
 class Whether
 	
 	def get_day(path)
-		@col3 = []
-		@col1 = []
+		col3 = []
+		col1 = []
 		File.open(path, "r").each_line do |line|
 		  	#puts line
 		  	data = line.split(/\s+/)
-		  	 @col3 << data[3] if (data[3] != nil && data[3].to_i > 0)
-		  	 @col1 << data[1] if (data[3] != nil && data[3].to_i > 0)
+		  	 col3 << data[3] if ( data[3].to_i > 0)
+		  	 col1 << data[1] if ( data[3].to_i > 0)
 		end
 
-		day_index =  @col3.index(@col3.min)
+		day_index = col3.index(col3.min)
 		
-		@col1[day_index.to_i]
+		col1[day_index.to_i]
 	end
 end
 
