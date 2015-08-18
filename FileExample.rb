2eac76eeb991040e31aa366bdcc35d8e92e29f58
v2s-Rakesh <rakesh.h@v2solutions.com>
2015-08-18 09:40:36 +0530

    modified file

--- FileExample.rb
+++ FileExample.rb
@@ -4,22 +4,22 @@ class Whether
 	
 	def get_day(path)
 		temp_diff = []
-		col1 = []
+		day = []
 		File.open(path, "r").each_line do |line|
 		  	#puts line
 		  	data = line.split(/\s+/)
 		  	 temp_diff << data[2].to_i-data[3].to_i if (data[1].to_i > 0 && data[3].to_i > 0)
-		  	 #col1 << data[1] if ( data[3].to_i > 0)
+		  	 day << data[1] if ( data[3].to_i > 0)
 		end
-
-		temp_diff[temp_diff.min]
+		temp_index = temp_diff.index(temp_diff.min)
+		day.at(temp_index)
 	end
 end
 
 class TestNumbers < Test::Unit::TestCase
     def test_line
     	file="/home/rakesh/Downloads/weather.txt"
-        assert_equal(22, Whether.new.get_day(file))
+        assert_equal("14", Whether.new.get_day(file))
     end
 
 end
