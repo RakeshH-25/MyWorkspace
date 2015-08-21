8d815991268963e8398d529199ea63537c828a4c
v2s-Rakesh <rakesh.h@v2solutions.com>
2015-08-21 13:25:59 +0530

    re-commit

---
+++ acessor.rb
@@ -0,0 +1,43 @@
+class Class
+  
+  def my_attr_accessor(*args)
+
+    args.each do |arg|
+      
+      self.class_eval("def #{arg};@#{arg};end")
+      
+      self.class_eval("def #{arg}=(val);@#{arg}=val;end")                      
+                      
+    end
+  end
+
+  def my_attr_reader(*args)
+
+    args.each do |arg|
+      
+      self.class_eval("def #{arg};@#{arg};end")
+                     
+    end
+  end
+
+  def my_attr_writer(*args)
+
+    args.each do |arg|
+      
+      self.class_eval("def #{arg}=(val);@#{arg}=val;end")  
+                     
+    end
+  end
+end
+
+class Person
+  #my_attr_accessor :name, :age  
+  my_attr_writer :name, :age 
+  my_attr_reader :name, :age 
+end
+
+person = Person.new
+person.name = "Rakesh"
+person.age = 25
+puts person.name
+puts person.age
