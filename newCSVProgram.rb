183f17f635723075d231110ccb53bda128863163
v2s-Rakesh <rakesh.h@v2solutions.com>
2015-08-24 15:11:26 +0530

    new CSVProgram file

---
+++ newCSVProgram.rb
@@ -0,0 +1,52 @@
+class CSVReader
+  require 'csv' 
+  def self.inherited(subclass)
+    @@filename = subclass
+    
+    @@arr_of_emp = CSV.read("./#{subclass}.csv", :headers => true).to_a
+    
+    @@arr_of_emp[0].each do |attr|
+    
+      # Adds accessor method which are the column headings of the csv.
+        self.class_eval("attr_accessor :#{attr}")
+        #Adds class method of the form find_by_<column_name>(value).  
+      self.class_eval("def self.find_by_#{attr}(val);e = emp_find(val);end") 
+
+    end   
+       
+  end
+
+  def self.emp_find(val)
+    employee = []
+    @@arr_of_emp.each do |row|
+      if row.to_a.include?(val)
+        emp = Employee.new
+          @@arr_of_emp[0].each_with_index do |attr, index|
+            emp.instance_variable_set("@#{attr}", row[index])
+          end
+        employee << emp
+      end
+    end
+    return employee
+  end
+
+  def method_missing(method_name,*args,&meth_bolck)
+    puts "method #{method_name} missing"
+
+  end
+end
+
+class Employee < CSVReader
+end
+
+emp=Employee.new
+emp.name = "rak"
+emp.age = 25
+emp.department = "it"
+p emp
+puts
+
+ p Employee.find_by_age("25")
+ puts
+
+ p Employee.find_by_name("Sowjanya")
