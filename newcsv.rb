f5ca96a5ed5473661f6f7dfe4fc7be15b5987adc
v2s-Rakesh <rakesh.h@v2solutions.com>
2015-08-27 14:57:31 +0530

    new csv solution

--- newcsv.rb
+++ newcsv.rb
@@ -2,31 +2,25 @@ class CSVReader
   require 'csv' 
 
   def self.inherited(subclass)
-    load_content(subclass)
-    @col_headers.each do |attr|
-      subclass.instance_eval("def self.find_by_#{attr}(valu);@col_name = \"#{attr}\";e = objects_with(valu.to_s,@col_name);end")
+    CSV.read("./#{subclass}.csv", :headers => true).headers.each do |attr|
+      subclass.instance_eval("def self.find_by_#{attr}(valu);@col_name = \"#{attr}\";objects_with(valu.to_s,@col_name);end")
       subclass.instance_eval("attr_accessor :#{attr}")
-    end  
-  end
-
-  def self.load_content(filename)
-    @file_data   = CSV.read("./#{filename}.csv", :headers => true).to_a
-    @col_headers = @file_data[0] 
+    end 
   end
 
   def self.objects_with(valu,col_name)
-    match = []
-    load_content(self)
-    @file_data.each do |row|
-      if row[@col_headers.index(col_name)] == valu
+    matching_objects = []
+    content = CSV.read("./#{self}.csv", :headers => true)
+    content.each do |row|
+      if row[col_name] == valu.to_s
         new_obj = self.new
-        @col_headers.each_with_index do |attr, index|
+        content.headers.each_with_index do |attr, index|
           new_obj.instance_variable_set("@#{attr}", row[index])
         end
-        match << new_obj
+        matching_objects << new_obj
       end
     end
-    match
+    matching_objects
   end
 end
 
@@ -39,7 +33,7 @@ end
 class Company < CSVReader
 end
 
-  emp=Employee.new
+emp=Employee.new
   emp.name = "rak"
   emp.age = 25
   emp.department = "it"
